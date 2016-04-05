53958c75f6a4135f83f3b8c8c68f10206b63f0dd
Derek Schneider <dschneider@covermymeds.com>
2016-04-04 20:40:25 -0400

    Remove unused capture group from IN clauses regex

--- lib/sql_footprint/sql_anonymizer.rb
+++ lib/sql_footprint/sql_anonymizer.rb
@@ -1,7 +1,7 @@
 module SqlFootprint
   class SqlAnonymizer
     GSUBS = {
-      /\sIN\s\((.*)\)/ => ' IN (values-redacted)'.freeze, # IN clauses
+      /\sIN\s\(.*\)/ => ' IN (values-redacted)'.freeze, # IN clauses
       /\s\=\s[0-9]+/ => ' = number-redacted'.freeze, # numbers
       /\s'.*'/ => " 'value-redacted'".freeze, # literal strings
     }.freeze
