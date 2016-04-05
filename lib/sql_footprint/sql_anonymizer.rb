7777f6b2c5748444447555b98beadc27f35255fc
Derek Schneider <dschneider@covermymeds.com>
2016-04-04 20:39:54 -0400

    Remove unused capture group from number gsub

--- lib/sql_footprint/sql_anonymizer.rb
+++ lib/sql_footprint/sql_anonymizer.rb
@@ -2,7 +2,7 @@ module SqlFootprint
   class SqlAnonymizer
     GSUBS = {
       /\sIN\s\((.*)\)/ => ' IN (values-redacted)'.freeze, # IN clauses
-      /\s\=\s([0-9]+)/ => ' = number-redacted'.freeze, # numbers
+      /\s\=\s[0-9]+/ => ' = number-redacted'.freeze, # numbers
       /\s'.*'/ => " 'value-redacted'".freeze, # literal strings
     }.freeze
 
